27fd16f856f3a61c586cf58da248fcbfc67eca36
achraf_nait_belkacem <achrafoonb@gmail.com>
2025-01-11 17:44:15 +0100

    update root

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,34 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import PopularMovies from './pages/PopularMovies'
+import NewMovies from './pages/NewMovies'
+import TopRatedMovies from './pages/TopRatedMovies'
+import UpcomingMovies from './pages/UpcomingMovies'
+import MovieDetails from './pages/MovieDetails'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <main>
-        <PopularMovies />
-      </main>
-      <Footer />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<PopularMovies />} />
+            <Route path="/nouveautes" element={<NewMovies />} />
+            <Route path="/top-rated" element={<TopRatedMovies />} />
+            <Route path="/a-venir" element={<UpcomingMovies />} />
+            <Route path="/film/:id" element={<MovieDetails />} />
+            <Route path="/connexion" element={<Login />} />
+            <Route path="/inscription" element={<Register />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   )
 }
 
